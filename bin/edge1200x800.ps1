5d318cad4a5b47fe038c03198196c52942a7f396
daichigoto <daichigoto@gmail.com>
2021-07-05 20:22:36 +0900

    updated

---
+++ bin/edge1200x800.ps1
@@ -0,0 +1,33 @@
+#
+#  Edgeウィンドウサイズを1200x800にするスクリプト
+#
+
+#========================================================================
+# Application name and geometry parameters
+#========================================================================
+$name = "msedge"	# application name
+$x = 0  		# x coordinate
+$y = 0			# y coordinate
+$widht = 1200 + 2	# width
+$height = 800 + 2	# heigth
+
+#========================================================================
+# Win32 API MoveWindows() function
+#========================================================================
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class Win32Api {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+    }
+"@
+
+#========================================================================
+# Windows resizing
+#========================================================================
+Get-Process -Name $name | ? { $_.MainWindowTitle -ne "" } | % {
+    [Win32Api]::MoveWindow($_.MainWindowHandle, $x, $y, $widht, $height, $true)
+} | Out-Null
