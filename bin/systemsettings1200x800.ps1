c3f8503f65224539c9ab8e2072b9df749c172ae1
daichigoto <daichigoto@gmail.com>
2021-07-08 16:35:54 +0900

    updated

---
+++ bin/systemsettings1200x800.ps1
@@ -0,0 +1,33 @@
+#
+#  設定ウィンドウサイズを1200x800にするスクリプト
+#
+
+#========================================================================
+# Window title and geometry parameters
+#========================================================================
+$title = "設定"		# window title
+$x = 0  		# x coordinate
+$y = 0			# y coordinate
+$widht = 1200		# width
+$height = 800		# heigth
+
+#========================================================================
+# Win32 API MoveWindows() function
+#========================================================================
+Add-Type @"
+    using System;
+    using System.Runtime.InteropServices;
+
+    public class Win32Api {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+    }
+"@
+
+#========================================================================
+# Windows resizing
+#========================================================================
+Get-Process | ? { $_.MainWindowTitle -eq $title } | % {
+    [Win32Api]::MoveWindow($_.MainWindowHandle, $x, $y, $widht, $height, $true)
+} | Out-Null
