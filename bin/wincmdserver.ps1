1bf36f7c76889a970276fbd26ae1b99c5a8e9a9d
daichigoto <daichigoto@gmail.com>
2021-07-20 12:55:55 +0900

    updated

--- bin/wincmdserver.ps1
+++ bin/wincmdserver.ps1
@@ -3,37 +3,60 @@
 #
 
 #========================================================================
-# コマンドが書き込まれるファイル
+# コマンド/クリップボートテキストが書き込まれるファイル
 #========================================================================
 $commandFilePath = "${HOME}/.wincmdserver_cmd"
+$clipbrdFilePath = "${HOME}/.wincmdserver_clip"
 
 #========================================================================
 # ファイルをチェックするインターバル時間[秒]
 #========================================================================
-$commandFileCheckInterval = 1.0
+$fileCheckInterval = 1.0
 
 #========================================================================
-# コマンドが書き込まれるファイルを監視して、コマンドが書き込まれた場合には
-# コマンドを実行してファイルをクリアする。
+# コマンド/クリップボートテキストが書き込まれるファイルを監視して、
+# コマンド/クリップボートテキストが書き込まれた場合には、コマンドを
+# 実行してファイルをクリアしたり、システムクリップボートへコピーして
+# ファイルをクリアする。
 #========================================================================
 while ($true) {
-    # ファイルが存在し、かつ、中身があるときに処理を行う
+    # コマンドファイルが存在し、かつ、中身があるときに中身を実行する
     if (Test-Path "$commandFilePath") {
 
         if (0 -lt (Get-ChildItem "$commandFilePath").Length) {
 
+            # ファイルに書き込まれたコマンドを実行
+            Invoke-Expression (cat "$commandFilePath") 
+
             # ログ出力
             $timestamp = Get-Date -format "yyyy/MM/dd HH:mm:ss"
             $timestamp + " - " + (cat "$commandFilePath")
-
-            # ファイルに書き込まれたコマンドを実行
-            Invoke-Expression (cat "$commandFilePath") 
 
             # ファイルの中身をクリア
             Clear-Content "$commandFilePath"
         }
     }
 
+    # クリップボートファイルが存在し、かつ、中身があるときにシステム
+		# クリップボードへコピーする
+    if (Test-Path "$clipbrdFilePath") {
+
+        if (0 -lt (Get-ChildItem "$clipbrdFilePath").Length) {
+
+            # システムクリップボードへテキストをコピー
+						$OutputEncoding = [Console]::OutputEncoding
+						cat "$clipbrdFilePath" | clip
+						$OutputEncoding = New-Object System.Text.UTF8Encoding
+
+            # ログ出力
+            $timestamp = Get-Date -format "yyyy/MM/dd HH:mm:ss"
+            $timestamp + " - copied to system clipboard"
+
+            # ファイルの中身をクリア
+            Clear-Content "$clipbrdFilePath"
+        }
+    }
+
     # 次のチェックまで指定秒間待機
-    Start-Sleep $commandFileCheckInterval
+    Start-Sleep $fileCheckInterval
 }
