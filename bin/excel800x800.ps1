a6324a59fff3990b6844ba874ef26dfa7b3415c9
daichigoto <daichigoto@gmail.com>
2021-07-22 11:17:16 +0900

    updated

--- bin/excel800x800.ps1
+++ bin/excel800x800.ps1
@@ -1,33 +1,5 @@
-#
 #  Excelウィンドウサイズを800x800にするスクリプト
-#
 
-#========================================================================
-# Application name and geometry parameters
-#========================================================================
-$name = "EXCEL"		# application name
-$x = 0  		# x coordinate
-$y = 0			# y coordinate
-$widht = 800 + 2	# width
-$height = 800 + 2	# heigth
+$cmd = ${PSScriptRoot} + "\window_resizer.ps1 EXCEL * 800 800"
 
-#========================================================================
-# Win32 API MoveWindows() function
-#========================================================================
-Add-Type @"
-    using System;
-    using System.Runtime.InteropServices;
-
-    public class Win32Api {
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
-    }
-"@
-
-#========================================================================
-# Windows resizing
-#========================================================================
-Get-Process -Name $name | ? { $_.MainWindowTitle -ne "" } | % {
-    [Win32Api]::MoveWindow($_.MainWindowHandle, $x, $y, $widht, $height, $true)
-} | Out-Null
+Invoke-Expression $cmd
