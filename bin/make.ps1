2ac3670a94dbfc37b978e05537352badbdcd3f33
daichigoto <daichigoto@gmail.com>
2021-08-14 13:23:16 +0900

    update

--- bin/make.ps1
+++ bin/make.ps1
@@ -1,8 +1,8 @@
 # BSD MakefileとWindows GNU Makefileを共存させるためのラッパースクリプト
 #
 if (Test-Path -PathType Leaf Makefile.win) {
-	make.exe -f Makefile.win
+	make.exe -f Makefile.win $Args
 }
 else {
-	make.exe 
+	make.exe $Args
 }
