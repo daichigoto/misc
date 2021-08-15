6235146ff95fca1c6facb6d02e5c39816c481aff
daichigoto <daichigoto@gmail.com>
2021-08-15 12:24:56 +0900

    Suiting for Windows

--- bin/make.ps1
+++ bin/make.ps1
@@ -1,5 +1,17 @@
+#=========================================================================
 # BSD MakefileとWindows GNU Makefileを共存させるためのラッパースクリプト
-#
+#=========================================================================
+
+# MSYS2 make.exeをインストール
+if (! (Test-Path -PathType Leaf C:\msys64\usr\bin\pacman.exe)) {
+	winget install MSYS2
+	C:\msys64\usr\bin\pacman.exe -Syu --noconfirm
+}
+if (! (Test-Path -PathType Leaf C:\msys64\usr\bin\make.exe)) {
+	pacman -S --noconfirm make
+}
+
+# Makefile.winがある場合にはこれを指定してmake.exeを実行
 if (Test-Path -PathType Leaf Makefile.win) {
 	make.exe -f Makefile.win $Args
 }
