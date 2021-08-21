17c81cf258f101847746adab1e6fd822e44085a3
daichigoto <daichigoto@gmail.com>
2021-08-21 16:55:40 +0900

    updated

--- bin/make.ps1
+++ bin/make.ps1
@@ -2,9 +2,20 @@
 # BSD MakefileとWindows GNU Makefileを共存させるためのラッパースクリプト
 #=========================================================================
 
-# MSYS2
+#=========================================================================
+# WindowsではGNUmakeを使用する
+#=========================================================================
+# MSYS2 GNUmake
 $make = "C:\msys64\usr\bin\make.exe"
 
+#=========================================================================
+# プラットフォームおよびコマンドセットをセットアップ
+#=========================================================================
+# MSYS2をインストール
+if (! (Test-Path -PathType Leaf C:\msys64\usr\bin\pacman.exe)) {
+	winget install MSYS2
+	C:\msys64\usr\bin\pacman.exe -Syu --noconfirm
+}
 # make.exeをインストール
 if (! (Test-Path -PathType Leaf $make)) {
 	pacman -S --noconfirm make
@@ -19,32 +30,3 @@ else {
 	$cmd = $make + " " + $Args
 	Invoke-Expression $cmd
 }
-
-
-## Cygwin note
-#$make = "C:\cygwin64\bin\make.exe"
-#
-## make.exeをインストール
-#if (! (Test-Path -PathType Leaf $make)) {
-#	echo "Install make"
-#}
-#
-## Makefile.winがある場合にはこれを指定してmake.exeを実行
-#if (Test-Path -PathType Leaf Makefile.win) {
-#	$cmd = $make + " -f Makefile.win " + $Args
-#	Invoke-Expression $cmd
-#}
-#else {
-#	$cmd = $make + " " + $Args
-#	Invoke-Expression $cmd
-#}
-
-# MSYS2 note
-# make.exeをインストール
-# if (! (Test-Path -PathType Leaf C:\msys64\usr\bin\pacman.exe)) {
-# 	winget install MSYS2
-# 	C:\msys64\usr\bin\pacman.exe -Syu --noconfirm
-# }
-# if (! (Test-Path -PathType Leaf C:\msys64\usr\bin\make.exe)) {
-# 	pacman -S --noconfirm make
-# }
