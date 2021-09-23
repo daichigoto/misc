5f43dbd16bc3b20dfe164f445c8cdb5b499335e3
daichigoto <daichigoto@gmail.com>
2021-09-23 19:03:56 +0900

    updated

--- bin/win.ps1
+++ bin/win.ps1
@@ -5,49 +5,47 @@
 #====================================================================
 
 #====================================================================
-# 引数を処理
+# サブコマンド抽出、引数を文字列へ変換
 #====================================================================
-Param(
-	[String]$SubCommand="",		# サブコマンド
-	[String]$SubCommandArg1="",	# サブコマンド引数1
-	[String]$SubCommandArg2="",	# サブコマンド引数2
-	[String]$SubCommandArg3="",	# サブコマンド引数3
-	[String]$SubCommandArg4="",	# サブコマンド引数4
-	[String]$SubCommandArg5="",	# サブコマンド引数5
-	[String]$SubCommandArg6="",	# サブコマンド引数6
-	[String]$SubCommandArg7="",	# サブコマンド引数7
-	[String]$SubCommandArg8="",	# サブコマンド引数8
-	[String]$SubCommandArg9=""	# サブコマンド引数9
-)
+$argstr=$Args -join ' '
+$subcmd=$Args[0]
+$subcmdarg1=$Args[1]
+$subcmdarg2=$Args[2]
+$subcmdargs=$Args[1..100] -join ' '
+
 
 #====================================================================
 # サブコマンドごとに処理を実行
 #====================================================================
-switch ($SubCommand)
+switch ($subcmd)
 {
 	"msedge" {
-		start Shell:AppsFolder'\'Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge $SubCommandArg1
+		$cmd='C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
+		& $cmd $subcmdarg1
 	}
 	"screenshotbrowser1200x800" {
-		& 'C:\Program Files\Mozilla Firefox\firefox.exe' $SubCommandArg1
+		$cmd='C:\Program Files\Mozilla Firefox\firefox.exe'
+		& $cmd $subcmdarg1
 		firefox1200x800.ps1
 	}
 	"screenshotbrowser1200x600" {
-		& 'C:\Program Files\Mozilla Firefox\firefox.exe' $SubCommandArg1
+		$cmd='C:\Program Files\Mozilla Firefox\firefox.exe'
+		& $cmd $subcmdarg1
 		firefox1200x600.ps1
 	}
 	"mail" {
-		& 'C:\Program Files\Mozilla Thunderbird\thunderbird.exe' -compose $SubCommandArg1
+		$cmd='C:\Program Files\Mozilla Thunderbird\thunderbird.exe'
+		& $cmd -compose $subcmdarg1
 	}
 	"clip" {
-		if ("" -eq $SubCommandArg1) {
-			clip.exe
+		if ($null -eq $subcmdarg1) {
+			$Input | clip.exe
 		}
 		else {
-			clip.exe $SubCommandArg1
+			Get-Content $subcmdarg1 | clip.exe
 		}
 	}
 	default {
-		Invoke-Expression ( $SubCommand + " " + $SubCommandArg1 + " " + $SubCommandArg2 + " " + $SubCommandArg3 + " " + $SubCommandArg4 + " " + $SubCommandArg5 + " " + $SubCommandArg6 + " " + $SubCommandArg7 + " " + $SubCommandArg8 + " " + $SubCommandArg9 )
+		Invoke-Expression $argstr
 	}
 }
