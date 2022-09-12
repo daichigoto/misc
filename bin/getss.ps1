f19685852305828aa60120da55315ebedd8a4918
daichigoto <daichigoto@gmail.com>
2022-09-12 15:22:08 +0900

    updated

--- bin/getss.ps1
+++ bin/getss.ps1
@@ -22,10 +22,11 @@ Param(
 $outfile="${env:HOME}/ss.png"
 
 #========================================================================
-# スクリーンショットの取得に利用するアプリケーション
+# スクリーンショットの取得に利用するアプリケーションほか
 #========================================================================
 $msedge='C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe'
 $snippingtool='SnippingTool.exe'
+$curl='C:\Windows\System32\curl.exe'
 
 #========================================================================
 # どの方法でスクリーンショットを取得するか判断
@@ -47,6 +48,99 @@ switch	-Wildcard ($URI)
 		# Snipping Toolを使って取得
 		#========================================================
 		$method='snippingtool'
+		break
+	}
+
+	default
+	{
+		$method='none'
+	}
+}
+
+#========================================================================
+# スクリーンショットを撮るリソース種類の特定
+#========================================================================
+$contenttype='none'
+switch	-Wildcard ($URI)
+{
+	#================================================================
+	# コンテンツの種類を特定
+	#================================================================
+	'http*.html'
+	{
+		$contenttype='text/html'
+		break
+	}
+	'http*.htm'
+	{
+		$contenttype='text/html'
+		break
+	}
+	'http*.shtml'
+	{
+		$contenttype='text/html'
+		break
+	}
+	'http*/'
+	{
+		$contenttype='text/html'
+		break
+	}
+	'http*.txt'
+	{
+		$contenttype='text/plain'
+		break
+	}
+	'http*.csv'
+	{
+		$contenttype='text/csv'
+		break
+	}
+	'http*csv=1'
+	{
+		$contenttype='text/csv'
+		break
+	}
+	'http*.pdf'
+	{
+		$contenttype='application/pdf'
+		break
+	}
+	'http*.zip'
+	{
+		$contenttype='application/zip'
+		break
+	}
+	default
+	{
+		$contenttype=(	& $curl		--location		`
+						-Ss -I			`
+						$URI			|
+				Select-String	"^Content-Type:"	)
+		break
+	}
+}
+
+#========================================================================
+# スクリーンショットが取れないWebリソースの場合、代わりに
+# http://example.com/ のスクリーンショットを取っておく
+#========================================================================
+switch	-Wildcard ($contenttype)
+{
+	#================================================================
+	# コンテンツの種類を特定
+	#================================================================
+	'*text/html*'
+	{
+		break
+	}
+	'*text/plain*'
+	{
+		break
+	}
+	'*/*'
+	{
+		$URI='http://example.com/'
 		break
 	}
 }
