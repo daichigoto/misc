3c14ec76db8b37bcf6b330c7f48a7b86d4d216f5
daichigoto <daichigoto@gmail.com>
2023-02-13 13:27:12 +0900

    updated

--- bin/firefox1200x600.ps1
+++ bin/firefox1200x600.ps1
@@ -4,6 +4,6 @@
 #  Firefoxコンテンツサイズを1200x600にするスクリプト
 #========================================================================
 
-$cmd = ${PSScriptRoot} + "\window_resize.ps1 firefox .* 1203 677"
+$cmd = ${PSScriptRoot} + "\window_resize.ps1 firefox .* 1206 687"
 
 Invoke-Expression $cmd
